63d9d77da6f614ae48421677a3620ac3aa697efb
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2015-04-12 22:14:06 -0700

    Indented registrations controller properly

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   protected 
 
-  def after_sign_up_path_for(resource)
-    new_user_profile_path(current_user)    
-  end
+    def after_sign_up_path_for(resource)
+      new_user_profile_path(current_user)    
+    end
 
-  def after_inactive_sign_up_path_for(resource)
-    "http://google.com"
-  end
+    def after_inactive_sign_up_path_for(resource)
+      root_path
+    end
 
 end
